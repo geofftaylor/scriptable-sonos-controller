cc8185e89a8205616c6887153f2d08e3c1ce06a1
Geoff Taylor <geoff@taylors.name>
2021-01-22 18:14:24 -0500

    Add Scriptable header

--- SonosSettings.js
+++ SonosSettings.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: yellow; icon-glyph: cog;
 // Change the `module.exports.sonosServerUrl` value to the IP address or name of the computer where the Sonos HTTP API server is running.
 // The value must include 'http://' or 'https://', an IP address or computer name, and a port number (5005 for HTTP or 5006 for HTTPS, unless you changed the server settings).
 module.exports.sonosServerUrl = 'http://192.168.1.2:5005/';
